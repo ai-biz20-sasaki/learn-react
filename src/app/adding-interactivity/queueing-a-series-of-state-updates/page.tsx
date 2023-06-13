89f0150389bfb76dbe560928f4fb07214f3bb6a3
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-13 19:42:04 +0900

    Add adding-interactivity/queueing-a-series-of-state-updates/example02

--- src/app/adding-interactivity/queueing-a-series-of-state-updates/page.tsx
+++ src/app/adding-interactivity/queueing-a-series-of-state-updates/page.tsx
@@ -2,7 +2,7 @@
 //https://react.dev/learn/queueing-a-series-of-state-updates
 import Link from 'next/link'
 import Example01 from './example01'
-//import Example02 from './example02'
+import Example02 from './example02'
 //import Example03 from './example03'
 //import Example04 from './example04'
 //import Challenge01 from './challenge01'
@@ -24,6 +24,8 @@ export default function Page() {
       </h2>
       <h3>Example01</h3>
       <Example01 />
+      <h3>Example02</h3>
+      <Example02 />
       <h2>
         <a
           className=""
