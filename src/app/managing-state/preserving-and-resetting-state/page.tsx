a853616d9d41b281835fecbe51b7b964df288f86
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-13 16:17:17 +0900

    Add managing-state/preserving-and-resetting-state/example06

--- src/app/managing-state/preserving-and-resetting-state/page.tsx
+++ src/app/managing-state/preserving-and-resetting-state/page.tsx
@@ -6,6 +6,7 @@ import Example02 from './example02'
 import Example03 from './example03'
 import Example04 from './example04'
 import Example05 from './example05'
+import Example06 from './example06'
 //import Challenge01 from './challenge01'
 //import Challenge02 from './challenge02'
 //import Challenge03 from './challenge03'
@@ -33,6 +34,8 @@ export default function Page() {
       <Example04 />
       <h3>Example05</h3>
       <Example05 />
+      <h3>Example06</h3>
+      <Example06 />
       <h2>
         <a
           className=""
