36c0e9581c65c70c9c633a169cd8b8bf9b8788ff
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-14 07:48:58 +0900

    Add adding-interactivity/updating-arrays-in-state/example04

--- src/app/adding-interactivity/updating-arrays-in-state/page.tsx
+++ src/app/adding-interactivity/updating-arrays-in-state/page.tsx
@@ -5,7 +5,7 @@ import Example01 from './example01'
 import Example0201 from './example0201'
 import Example0202 from './example0202'
 import Example03 from './example03'
-//import Example04 from './example04'
+import Example04 from './example04'
 //import Example05 from './example05'
 //import Challenge01 from './challenge01'
 //import Challenge02 from './challenge02'
@@ -32,6 +32,8 @@ export default function Page() {
       <Example0202 />
       <h3>Example03</h3>
       <Example03 />
+      <h3>Example04</h3>
+      <Example04 />
       <h2>
         <a
           className=""
