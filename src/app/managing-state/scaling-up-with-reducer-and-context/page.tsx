ec3ae9c53b18bb7142f21d7163cf872588a7fe0c
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-14 11:10:28 +0900

    Add managing-state/scaling-up-with-reducer-and-context/example05

--- src/app/managing-state/scaling-up-with-reducer-and-context/page.tsx
+++ src/app/managing-state/scaling-up-with-reducer-and-context/page.tsx
@@ -5,6 +5,7 @@ import Example01 from './example01'
 import Example02 from './example02'
 import Example03 from './example03'
 import Example04 from './example04'
+import Example05 from './example05'
 //import Challenge01 from './challenge01'
 //import Challenge02 from './challenge02'
 //import Challenge03 from './challenge03'
@@ -30,6 +31,8 @@ export default function Page() {
       <Example03 />
       <h3>Example04</h3>
       <Example04 />
+      <h3>Example05</h3>
+      <Example05 />
 
       {/*
       <h2>
