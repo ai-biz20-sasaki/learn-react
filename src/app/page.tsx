165b88910c25ffa36bb36fd0692d443c0ca31617
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-22 08:38:50 +0900

    ルートページのリンクを修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,19 +19,19 @@ export default function Home() {
         <li>Importing and Exporting Components|コンポーネントのインポートとエクスポート</li>
         <li>Writing Markup with JSX|JSXでマークアップを書く</li>
         <li>
-          <Link href="/describing-the-ui/js-in-jsx-with-curly-braces">
+          <Link href="/01-describing-the-ui/04-js-in-jsx-with-curly-braces">
             JavaScript in JSX with Curly Braces|中括弧を使用した JSX の JavaScript
           </Link>
         </li>
         <li>
-          <Link href="/describing-the-ui/passing-props-to-a-component">
+          <Link href="/01-describing-the-ui/05-passing-props-to-a-component">
             Passing Props to a Component|プロパティをコンポーネントに渡す
           </Link>
         </li>
         <li>Conditional Rendering|条件付きレンダリング</li>
         <li>Rendering Lists|レンダリングリスト</li>
         <li>
-          <Link href="/describing-the-ui/keeping-components-pure">
+          <Link href="/01-describing-the-ui/08-keeping-components-pure">
             Keeping Components Pure|コンポーネントを純粋に保つ
           </Link>
         </li>
@@ -40,33 +40,33 @@ export default function Home() {
       <p>Adding Interactivity|インタラクティブ性の追加</p>
       <ul>
         <li>
-          <Link href="/adding-interactivity/responding-to-events">
+          <Link href="/02-adding-interactivity/01-responding-to-events">
             Responding to Events|イベントへの応答
           </Link>
         </li>
         <li>
-          <Link href="/adding-interactivity/state-a-components-memory">
+          <Link href="/02-adding-interactivity/02-state-a-components-memory">
             State: A Component's Memory|状態: コンポーネントのメモリ
           </Link>
         </li>
         <li>Render and Commit|レンダリングとコミット</li>
         <li>
-          <Link href="/adding-interactivity/state-as-a-snapshot">
+          <Link href="/02-adding-interactivity/04-state-as-a-snapshot">
             State as a Snapshot|スナップショットとしての状態
           </Link>
         </li>
         <li>
-          <Link href="/adding-interactivity/queueing-a-series-of-state-updates">
+          <Link href="/02-adding-interactivity/05-queueing-a-series-of-state-updates">
             Queueing a Series of State Updates|一連の状態更新をキューに入れる
           </Link>
         </li>
         <li>
-          <Link href="/adding-interactivity/updating-objects-in-state">
+          <Link href="/02-adding-interactivity/06-updating-objects-in-state">
             Updating Objects in State|状態内のオブジェクトの更新
           </Link>
         </li>
         <li>
-          <Link href="/adding-interactivity/updating-arrays-in-state">
+          <Link href="/02-adding-interactivity/07-updating-arrays-in-state">
             Updating Arrays in State|状態内の配列を更新する
           </Link>
         </li>
@@ -75,37 +75,37 @@ export default function Home() {
       <p>Managing State|状態の管理</p>
       <ul>
         <li>
-          <Link href="/managing-state/reacting-to-input-with-state">
+          <Link href="/03-managing-state/01-reacting-to-input-with-state">
             Reacting to Input with State|状態による入力への反応
           </Link>
         </li>
         <li>
-          <Link href="/managing-state/choosing-the-state-structure">
+          <Link href="/03-managing-state/02-choosing-the-state-structure">
             Choosing the State Structure|状態構造の選択
           </Link>
         </li>
         <li>
-          <Link href="/managing-state/sharing-state-between-components">
+          <Link href="/03-managing-state/03-sharing-state-between-components">
             Sharing State Between Components|コンポーネント間での状態の共有
           </Link>
         </li>
         <li>
-          <Link href="/managing-state/preserving-and-resetting-state">
+          <Link href="/03-managing-state/04-preserving-and-resetting-state">
             Preserving and Resetting State|状態の保存とリセット
           </Link>
         </li>
         <li>
-          <Link href="/managing-state/extracting-state-logic-into-a-reducer">
+          <Link href="/03-managing-state/05-extracting-state-logic-into-a-reducer">
             Extracting State Logic into a Reducer|状態ロジックを Reducer に抽出する
           </Link>
         </li>
         <li>
-          <Link href="/managing-state/passing-data-deeply-with-context">
+          <Link href="/03-managing-state/06-passing-data-deeply-with-context">
             Passing Data Deeply with Context|コンテキストに応じてデータを深く渡す
           </Link>
         </li>
         <li>
-          <Link href="/managing-state/scaling-up-with-reducer-and-context">
+          <Link href="/03-managing-state/07-scaling-up-with-reducer-and-context">
             Scaling Up with Reducer and Context|Reducer と Context によるスケールアップ
           </Link>
         </li>
